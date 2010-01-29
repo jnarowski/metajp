74c8c4b6c44afd948826e0607b61013e404eeb32
John Paul Narowski <jnarowski@gmail.com>
2010-01-29 12:04:17 -0500

    version bump to 0.2.0

--- lib/metajp.rb
+++ lib/metajp.rb
@@ -9,7 +9,7 @@ require File.dirname(__FILE__) + '/metajp/shared/acts_as_invitable/model.rb'
 
 module Metajp
   
-  VERSION = '0.1.0'
+  VERSION = '0.2.0'
   
   #----------------------------------------------------------------
   # extensions for the controller
